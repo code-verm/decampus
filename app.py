e801cdfcb72e045a32a4ae0a8d3b1341a6078c89
code-grey <adrishbora@gmail.com>
2024-01-16 21:45:46 +0530

    fixed the circular import issue in maapp.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@
 
 from flask import Flask
 from models import User, Book  # Import your models once they're defined
-from routes import *  # Import all routes from the routes file
+# from routes import *  # Import all routes from the routes file
 
 app = Flask(__name__)
 
@@ -22,3 +22,6 @@ app = Flask(__name__)
 
 if __name__ == "__app__":
     app.run(host='0.0.0.0', port=81, debug=True)
+from routes import *  # Import all routes from the routes file
+
+
