74cc66d460c83efa871613aaad25e868cd09b275
code-grey <adrishbora@gmail.com>
2024-01-16 23:30:47 +0530

    removed unnecessary comments

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     return 'decampus'
-
-
-# # if __name__ == '__main__':
-# app.run(host='0.0.0.0', port=81, debug=True)
-
 
 from flask import Flask
-from models import User, Book  # Import your models once they're defined
-# from routes import *  # Import all routes from the routes file
+from models import User, Book 
 
 app = Flask(__name__)
 
