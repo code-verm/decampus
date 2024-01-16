4fce3a644265fe691f5e3017aa4716354b46c2b3
code-grey <adrishbora@gmail.com>
2024-01-16 23:33:09 +0530

    fixed the cyclic import problem in app.py

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     return 'decampus'
-
-
-# # if __name__ == '__main__':
-# app.run(host='0.0.0.0', port=81, debug=True)
-
-
 from flask import Flask
 from models import User, Book  # Import your models once they're defined
-from routes import *  # Import all routes from the routes file
+#from routes import *  # Import all routes from the routes file
 
 app = Flask(__name__)
 
@@ -22,3 +8,5 @@ app = Flask(__name__)
 
 if __name__ == "__app__":
     app.run(host='0.0.0.0', port=81, debug=True)
+
+from routes import *
