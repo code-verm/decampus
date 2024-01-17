e78725de8e1e2a129552e51afad4d1bfe1bef8c5
code-grey <adrishbora@gmail.com>
2024-01-17 12:00:54 +0530

    removed the secrets

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from models import User, Book
 from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Import other modules here
 
+
+
+
 if __name__ == "__app__":
     app.run(host='0.0.0.0', port=81, debug=True)
-app.config["MONGO_URI"] = "mongodb+srv://adrishbora:fIrFnJafGOLS3gte@decampus-test-db-0.zybncgz.mongodb.net/?retryWrites=true&w=majority"
-
-mongo = PyMongo(app)
 
 from routes import *  # Import all routes from the routes file
 
--- routes.py
+++ routes.py
@@ -1,5 +1,12 @@
 from app import app
+from flask import Flask, render_template
+@app.route('/')
+#def home():
+#    return "Hello from Decampus!"
+def index():
+  # Example of fetching data for the homepage
+    return render_template('index.html') #, books=books)  # Pass data to the template
 
-@app.route("/")
-def home():
-    return "Hello from Decampus!"
+#@app.route('/books')
+#def books():
+ #   return render_template('books.html')
