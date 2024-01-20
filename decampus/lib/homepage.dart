1c4b70d3e1dd333dfb923c398f950bfbea2cf252
code-grey <adrishbora@outlook.com>
2024-01-21 00:05:18 +0530

    added loading_screen.dart, homepage.dart and login_signup_screen.dart, and modified main.dart

---
+++ decampus/lib/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Decampus'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add a list view to display available books
+            // Add a search bar for filtering books
+            // Add a button to list books
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ decampus/lib/login_signup_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginSignupScreen extends StatelessWidget {
+  const LoginSignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add input fields for email/username and password
+            // Add buttons for login and signup actions
+          ],
+        ),
+      ),
+    );
+  }
+}
